507e4b291d82b222dadabc47ddaeed9c442e0e47
GabriCastillo <72267459+GabriCastillo@users.noreply.github.com>
2021-12-11 19:59:34 +0100

    Actualizacion consultasBBDD
    Añadi 3 metodos, 2 para las consultas de los respectivos reportes, y 1 para la direccion

--- src/bbdd/ConsultasBBDD.java
+++ src/bbdd/ConsultasBBDD.java
@@ -11,6 +11,7 @@ public class ConsultasBBDD {
 	private Connection conexion = c.conectar();
 	private Statement stm = null;
 	private ResultSet rs = null; 
+	private String semanal ="select E.numeroCuenta, P.apellido, P.nombre, C.id_direccion, C.numeroIdentificacion, P.fechaNacimiento from Cliente C, CuentaEbury E, Persona P where E.propietario = C.id and  E.estadoCuenta != 'cerrada' and E.estadoCuenta = 'activa' and  TIMESTAMPDIFF (YEAR,E.fechaApertura,curdate())<=5";
 	
 	
 	public ResultSet obtenerClientes() {
@@ -20,7 +21,7 @@ public class ConsultasBBDD {
 			rs = stm.executeQuery("select * from Cliente");
 			
 			/*
-			 * AsÃ­ se recorrerÃ­a el resultSet y se sacarÃ­an por pantalla todos los clientes de la bbdd
+			 * Así se recorrería el resultSet y se sacarían por pantalla todos los clientes de la bbdd
 			 * 
 			
 			while(rs.next()) {
@@ -37,6 +38,95 @@ public class ConsultasBBDD {
 			
 		} catch (SQLException e) {
 			
+			e.printStackTrace();
+		}
+		return rs;
+	}
+	
+	public ResultSet reporteInicial() {
+		rs = null;
+		try {
+			stm = conexion.createStatement();
+			rs = stm.executeQuery("SELECT E.numeroCuenta, P.apellido, P.nombre, C.id_direccion, C.numeroIdentificacion, P.fechaNacimiento "
+					+				"FROM Cliente C, CuentaEbury E, Persona P "
+					+ 				"WHERE E.propietario = C.id");
+			
+			/*
+			 * Así se recorrería el resultSet y se sacarían por pantalla todos los clientes de la bbdd
+			 * 
+			
+			while(rs.next()) {
+				int id = rs.getInt(1);
+				String numeroIdentificacion = rs.getString(2);
+				String estado = rs.getString(3);
+				String fechaInicio = rs.getDate(4).toString();
+				int direccion = rs.getInt(5);
+				
+				System.out.println(id + " - " + numeroIdentificacion + " - " + estado + " - " + fechaInicio + " - " + direccion);
+			}
+			
+			*/
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return rs;
+	}
+	
+	public ResultSet obtenerDireccion(int id) {
+		rs = null;
+		try {
+			stm = conexion.createStatement();
+			rs = stm.executeQuery("SELECT * "
+					+ 				"FROM Direccion "
+					+ 				"WHERE id =" +id);
+			
+			/*
+			 * Así se recorrería el resultSet y se sacarían por pantalla todos los clientes de la bbdd
+			 * 
+			
+			while(rs.next()) {
+				int id = rs.getInt(1);
+				String numeroIdentificacion = rs.getString(2);
+				String estado = rs.getString(3);
+				String fechaInicio = rs.getDate(4).toString();
+				int direccion = rs.getInt(5);
+				
+				System.out.println(id + " - " + numeroIdentificacion + " - " + estado + " - " + fechaInicio + " - " + direccion);
+			}
+			
+			*/
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return rs;
+	}
+	
+	public ResultSet reporteSemanal() {
+		rs = null;
+		try {
+			stm = conexion.createStatement();
+			rs = stm.executeQuery(semanal);
+			
+			/*
+			 * Así se recorrería el resultSet y se sacarían por pantalla todos los clientes de la bbdd
+			 * 
+			
+			while(rs.next()) {
+				int id = rs.getInt(1);
+				String numeroIdentificacion = rs.getString(2);
+				String estado = rs.getString(3);
+				String fechaInicio = rs.getDate(4).toString();
+				int direccion = rs.getInt(5);
+				
+				System.out.println(id + " - " + numeroIdentificacion + " - " + estado + " - " + fechaInicio + " - " + direccion);
+			}
+			
+			*/
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return rs;
