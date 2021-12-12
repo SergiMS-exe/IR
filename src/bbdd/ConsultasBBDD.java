8606ac4575639aee39b41238b4c852b2790f890a
GabriCastillo <72267459+GabriCastillo@users.noreply.github.com>
2021-12-12 20:04:57 +0100

    actualizacion de consultas
    consultas para obtener personas, personas relacionadas a una cuenta

--- src/bbdd/ConsultasBBDD.java
+++ src/bbdd/ConsultasBBDD.java
@@ -11,7 +11,8 @@ public class ConsultasBBDD {
 	private Connection conexion = c.conectar();
 	private Statement stm = null;
 	private ResultSet rs = null; 
-	private String semanal ="select E.numeroCuenta, P.apellido, P.nombre, C.id_direccion, C.numeroIdentificacion, P.fechaNacimiento from Cliente C, CuentaEbury E, Persona P where E.propietario = C.id and  E.estadoCuenta != 'cerrada' and E.estadoCuenta = 'activa' and  TIMESTAMPDIFF (YEAR,E.fechaApertura,curdate())<=5";
+	private String personaRel = "select from PersonaRelacionada pr, Cliente c, CuentaEbury e, PersonaRelacionadaCliente prcl, PersonaRelacionadaCuenta prc where c.id=prcl.id_cliente and prcl.id_personaRelacionada= pr.id and pr.id=prc.idPersona and prc.idCuenta=";
+	private String semanal ="select E.numeroCuenta, P.apellido, P.nombre, C.id_direccion, C.numeroIdentificacion, P.fechaNacimiento,E.id from Cliente C, CuentaEbury E, Persona P where E.propietario = C.id and  E.estadoCuenta != 'cerrada' and E.estadoCuenta = 'activa' and  TIMESTAMPDIFF (YEAR,E.fechaApertura,curdate())<=5";
 	
 	
 	public ResultSet obtenerClientes() {
@@ -43,13 +44,11 @@ public class ConsultasBBDD {
 		return rs;
 	}
 	
-	public ResultSet reporteInicial() {
+	public ResultSet obtenerPersona(int id) {
 		rs = null;
 		try {
 			stm = conexion.createStatement();
-			rs = stm.executeQuery("SELECT E.numeroCuenta, P.apellido, P.nombre, C.id_direccion, C.numeroIdentificacion, P.fechaNacimiento "
-					+				"FROM Cliente C, CuentaEbury E, Persona P "
-					+ 				"WHERE E.propietario = C.id");
+			rs = stm.executeQuery("select * from Persona where id =" + id);
 			
 			/*
 			 * Así se recorrería el resultSet y se sacarían por pantalla todos los clientes de la bbdd
@@ -67,6 +66,35 @@ public class ConsultasBBDD {
 			
 			*/
 			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return rs;
+	}
+	
+	public ResultSet personasRelacionadas(int id) {
+		rs = null;
+		try {
+			stm = conexion.createStatement();
+			rs = stm.executeQuery("select *\n"
+					+ "from PersonaRelacionada pr, Cliente c, CuentaEbury e, PersonaRelacionadaCliente prcl, PersonaRelacionadaCuenta prc\n"
+					+ "where c.id=prcl.id_cliente and prcl.id_personaRelacionada= pr.id and pr.id=prc.idPersona and prc.idCuenta=" + id);		
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return rs;
+	}
+	
+	public ResultSet reporteInicial() {
+		rs = null;
+		try {
+			stm = conexion.createStatement();
+			rs = stm.executeQuery("SELECT E.numeroCuenta, P.apellido, P.nombre, C.id_direccion, C.numeroIdentificacion, P.fechaNacimiento,E.id "
+					+				"FROM Cliente C, CuentaEbury E, Persona P "
+					+ 				"WHERE E.propietario = C.id");
+	
 		} catch (SQLException e) {
 			
 			e.printStackTrace();
@@ -81,23 +109,7 @@ public class ConsultasBBDD {
 			rs = stm.executeQuery("SELECT * "
 					+ 				"FROM Direccion "
 					+ 				"WHERE id =" +id);
-			
-			/*
-			 * Así se recorrería el resultSet y se sacarían por pantalla todos los clientes de la bbdd
-			 * 
-			
-			while(rs.next()) {
-				int id = rs.getInt(1);
-				String numeroIdentificacion = rs.getString(2);
-				String estado = rs.getString(3);
-				String fechaInicio = rs.getDate(4).toString();
-				int direccion = rs.getInt(5);
-				
-				System.out.println(id + " - " + numeroIdentificacion + " - " + estado + " - " + fechaInicio + " - " + direccion);
-			}
-			
-			*/
-			
+
 		} catch (SQLException e) {
 			
 			e.printStackTrace();
@@ -110,22 +122,7 @@ public class ConsultasBBDD {
 		try {
 			stm = conexion.createStatement();
 			rs = stm.executeQuery(semanal);
-			
-			/*
-			 * Así se recorrería el resultSet y se sacarían por pantalla todos los clientes de la bbdd
-			 * 
-			
-			while(rs.next()) {
-				int id = rs.getInt(1);
-				String numeroIdentificacion = rs.getString(2);
-				String estado = rs.getString(3);
-				String fechaInicio = rs.getDate(4).toString();
-				int direccion = rs.getInt(5);
-				
-				System.out.println(id + " - " + numeroIdentificacion + " - " + estado + " - " + fechaInicio + " - " + direccion);
-			}
-			
-			*/
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
